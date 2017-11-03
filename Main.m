f254b730df747dd70f1f7005eaa135ef63e8a3ed
Grifo <leitaogrifo@gmail.com>
2017-11-03 22:45:40 +0000

    almost final

--- Main.m
+++ Main.m
@@ -17,14 +17,14 @@ if ext== '.wav'
     [H,Z,bincounts]=entropia(fonteInf,alfabeto);
     fprintf('Entropia de %s: %.4f\n',filename,H);
     
-% 
-%     pause(2);
-%     
-%     Lmed=exe4(bincounts,Z);
-%     fprintf('Comprimento medio de bits por simbolo: %.4f\n',Lmed);
-%     
-%     [Entropia2a2]=entropia2a2(fonteInf,info.BitsPerSample);
-%     fprintf('Entropia 2 a 2: %.4f\n',Entropia2a2);
+
+    pause(2);
+    
+    Lmed=exe4(bincounts,Z);
+    fprintf('Comprimento medio de bits por simbolo: %.4f\n',Lmed);
+    
+    [Entropia2a2]=entropia2a2(fonteInf,info.BitsPerSample,filename);
+    fprintf('Entropia 2 a 2: %.4f\n',Entropia2a2);
 
     
 elseif ext == '.bmp'
--- entropia2a2.m
+++ entropia2a2.m
@@ -1,8 +1,11 @@
-function [ H ] = entropia2a2(fonte,sllbits)
+function [ H ] = entropia2a2(fonte,sllbits,filename)
     
     
     [fonteInf,Alf]=shiftsimbolos(fonte,sllbits);
-    %histogram(fonteInf,Alf);
+    histogram(fonteInf,Alf);
+    ylabel('Numero de Ocorrências');
+    xlabel('Alfabeto');
+    title(filename);
     [H,Z,bincounts]=entropia(fonteInf,Alf); 
     H=H/2;
 
